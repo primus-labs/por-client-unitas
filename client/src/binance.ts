14a3e2fb4e6d01b5b4ba1b66082d7d36e0508b5f
ujnss <unlinunlin@163.com>
2025-12-11 09:09:20 +0000

    update program: use sdk datasource

--- client/src/binance.ts
+++ client/src/binance.ts
@@ -1,56 +1,22 @@
-
-import {
-  getBinanceAccounts,
-  makeBinanceRequestParams, makeBinanaceOrigRequests,
-  makeBinanceSpotRequestParams, makeBinanaceSpotOrigRequests
-} from "./utils";
-import { ZkTLSClient, ProverClient, saveToFile } from "@primuslabs/por-client-sdk";
-
-
-function getBinanaceRequestParams() {
-  const accounts = getBinanceAccounts();
-  // console.log('accounts', accounts);
-  const origRequests = makeBinanaceOrigRequests(accounts);
-  // console.log('origRequests', origRequests);
-  const { requests, responseResolves } = makeBinanceRequestParams(origRequests);
-  // console.log('requests', requests);
-  // console.log('responseResolves', responseResolves);
-  return { requests, responseResolves };
-}
-
-function getBinanaceSpotRequestParams() {
-  const accounts = getBinanceAccounts();
-  // console.log('accounts', accounts);
-  const origRequests = makeBinanaceSpotOrigRequests(accounts);
-  // console.log('origRequests', origRequests);
-  const { requests, responseResolves } = makeBinanceSpotRequestParams(origRequests);
-  // console.log('requests', requests);
-  // console.log('responseResolves', responseResolves);
-  return { requests, responseResolves };
-}
+import { DataSource, ZkTLSClient, ProverClient, saveToFile } from "@primuslabs/por-client-sdk";
 
 async function main() {
   console.log(`Now: ${new Date()}`);
-  // Configure at least one or more pairs of API_KEY and API_SECRET
-  // in .env using `BINANCE_API_KEY{i}` and `BINANCE_API_SECRET{i}`.
   try {
+    const ds = new DataSource.Binance();
     let data1: any = null;
     let data2: any = null;
     const zktlsClient = new ZkTLSClient();
     {
-      const { requests, responseResolves } = getBinanaceRequestParams();
-      data1 = await zktlsClient.doZkTLS(requests, responseResolves, {
-        requestParamsCallback: getBinanaceRequestParams,
-      });
+      const requestParams = ds.getUnifiedAccountRequests();
+      data1 = await zktlsClient.doZkTLS(requestParams);
       if (data1 && data1.attestationData) {
         saveToFile("attestation1.json", JSON.stringify(data1.attestationData));
       }
     }
     {
-      const { requests, responseResolves } = getBinanaceSpotRequestParams();
-      data2 = await zktlsClient.doZkTLS(requests, responseResolves, {
-        requestParamsCallback: getBinanaceSpotRequestParams,
-      });
+      const requestParams = ds.getSpotAccountRequests();
+      data2 = await zktlsClient.doZkTLS(requestParams);
       if (data2 && data2.attestationData) {
         saveToFile("attestation2.json", JSON.stringify(data2.attestationData));
       }
